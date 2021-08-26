8502c0b0619ad6b3e8ad72e98e0c708a7ca53fd9
Rauchendraser <andreasr@ppedv.de>
2021-08-26 16:41:01 +0200

    Seiten Blöcke MAXDOP Partitionierung

---
+++ SQL Perf_Tuning/SQL Perf_Tuning/08_Dateigruppe.sql
@@ -0,0 +1,16 @@
+--Dateigruppe
+
+create table test(id int) on HOT--kursdb2.ndf
+
+
+--Dateigruppen sind ein Alias für den Ort, wo SQL Server
+--weiter Datendateien abgelegt 
+
+--wie können wir Tabellen verschieben von einer DGruppe (PIRMARY)
+--auf eine andere DGruppe
+
+--Über Entwurfsansicht kann man das machen, aber Vorsicht--> Beziehungen..
+--alle Tabellen werden gesperrt
+
+
+
